0e02eb126b9e17747389bd8068bc978541dfeff0
SSlayer369 <130920448+SSlayer369@users.noreply.github.com>
2025-03-04 00:41:34 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Iniciar el servidor VNC
-vncserver :1 -geometry 800x1280 -depth 24
+vncserver :1 -geometry 1366x768 -depth 24
 
 # Iniciar noVNC
 /opt/noVNC/utils/novnc_proxy --vnc localhost:5901 --listen $PORT
